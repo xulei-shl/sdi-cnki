120c0bc744c0c0a9e5e8393f5a667d39d2c5abad
xulei-shl <xl_0527@163.com>
2026-05-28 08:48:53 +0800

    未登录保护

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -1,17 +1,20 @@
 import { Outlet } from 'react-router-dom'
+import { ProtectedRoute } from '@/components/protected-route'
 import { Sidebar } from './sidebar'
 import { Header } from './header'
 
 export function AppLayout() {
   return (
-    <div className="h-screen flex overflow-hidden">
-      <Sidebar />
-      <div className="flex-1 flex flex-col min-w-0">
-        <Header />
-        <main className="flex-1 overflow-hidden">
-          <Outlet />
-        </main>
+    <ProtectedRoute>
+      <div className="h-screen flex overflow-hidden">
+        <Sidebar />
+        <div className="flex-1 flex flex-col min-w-0">
+          <Header />
+          <main className="flex-1 overflow-hidden">
+            <Outlet />
+          </main>
+        </div>
       </div>
-    </div>
+    </ProtectedRoute>
   )
 }
